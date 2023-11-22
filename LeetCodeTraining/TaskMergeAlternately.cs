958d70c2c6f9a516a5fbe6e80570ec3bd69d6793
Kvazark <e.klebleeva@bpo.travel>
2023-11-22 15:33:01 +0300

    Initial commit

---
+++ LeetCodeTraining/TaskMergeAlternately.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeTraining;
+
+public class TaskMergeAlternately
+{
+    public string MergeAlternately(string word1, string word2) {
+        if(1<=word1.Length && word2.Length<=100 && 1<=word1.Length && word2.Length<=100){
+            var maxLenghtWord = word1.Length>word2.Length ? word1.Length:word2.Length;
+
+
+
+
+        }
+
+        return "";
+    }
+}
